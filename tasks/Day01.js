f4a2b3f1707a186a7d069bade52143525ad1b074
Stefani Ilieva <stefani.ilieva@a1.bg>
2024-11-07 14:21:10 +0200

    solve part two of day 01

--- tasks/Day01.js
+++ tasks/Day01.js
@@ -9,28 +9,79 @@ const Day01 = () => {
     .split("\n")
     .map((line) => line.trim());
 
-  function getCalibrationValues(lines) {
-    const values = lines.map((line) => {
-      const firstDigit = line.match(/\d/);
-      const lastDigit = line.match(/\d(?=[^0-9]*$)/);
 
-      if (firstDigit && lastDigit) {
-        return parseInt(firstDigit[0] + lastDigit[0]);
+  const words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  const reversedWords = words.map(word => word.split("").reverse().join(""));
+
+  const wordToDigit = {
+    "zero": "0", "one": "1", "two": "2", "three": "3", "four": "4",
+    "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"
+  };
+
+  function findFirstDigit(line) {
+    let minIndex = line.length;
+    let firstDigit = null;
+
+    const digitMatch = line.match(/\d/);
+    if (digitMatch) {
+      minIndex = digitMatch.index;
+      firstDigit = digitMatch[0];
+    }
+
+
+    for (let i = 0; i < words.length; i++) {
+      const index = line.indexOf(words[i]);
+      if (index !== -1 && index < minIndex) {
+        minIndex = index;
+        firstDigit = wordToDigit[words[i]];
       }
-      return null;
-    });
-
-    return values.filter((value) => value !== null);
+    }
+    return firstDigit;
   }
 
-  const calibrationValues = getCalibrationValues(lines);
-  const sumOfCalibrationValues = calibrationValues.reduce(
-    (sum, value) => sum + value,
-    0
-  );
+  function findLastDigit(line) {
+    const reversedLine = line.split("").reverse().join("");
+    let minIndex = reversedLine.length;
+    let lastDigit = null;
 
-  console.log("calibrationValues", calibrationValues);
-  console.log("sumOfCalibrationValues", sumOfCalibrationValues);
+    const digitMatch = reversedLine.match(/\d/);
+    if (digitMatch) {
+      minIndex = digitMatch.index;
+      lastDigit = digitMatch[0];
+    }
+
+    for (let i = 0; i < reversedWords.length; i++) {
+      const index = reversedLine.indexOf(reversedWords[i]);
+      if (index !== -1 && index < minIndex) {
+        minIndex = index;
+        lastDigit = wordToDigit[words[i]];
+      }
+    }
+    return lastDigit;
+  }
+
+  function getCalibrationValues(lines) {
+    return lines.map((line) => {
+      const first = findFirstDigit(line);
+      const last = findLastDigit(line);
+
+      if (first !== null && last !== null) {
+        const calibrationValue = parseInt(first + last, 10);
+        console.log(`Line: "${line}" -> Calibration value: ${calibrationValue}`);
+        return calibrationValue;
+      }
+      return null;
+    }).filter(value => value !== null);
+  }
+
+
+  const calibrationValues = getCalibrationValues(lines);
+
+
+  const sumOfCalibrationValues = calibrationValues.reduce((sum, value) => sum + value, 0);
+
+  console.log("Calibration Values:", calibrationValues);
+  console.log("Sum of Calibration Values:", sumOfCalibrationValues);
 };
 
 Day01();
